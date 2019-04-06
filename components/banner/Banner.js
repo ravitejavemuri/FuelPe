86f3f40c3724baff8bb1cd71314f54429983d176
Sanif Himani <25054137+sanifhimani@users.noreply.github.com>
2019-04-06 21:49:40 +0530

    Add files via upload

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,1 +1,29 @@
+import React, { Component } from "react";
+import { 
+    View,
+    StyleSheet,
+    Image,
+} from "react-native";
+import colors from "../../styles/colors";
 
+class Banner extends Component {
+    
+    render() {
+        const containerHeight = 150;
+        const childHeight = 150;
+        const margin = (containerHeight - childHeight) / 2;
+        return (
+            <View style={{ flex: 1, height: childHeight, margin: margin }}>
+                <Image source={this.props.imageUri} style={{ flex: 1, height: 50, resizeMode: 'contain', backgroundColor: colors.grey }} />
+            </View>
+        );
+    }
+}
+
+export default Banner;
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+});
