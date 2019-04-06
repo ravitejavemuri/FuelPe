46c8ba41ba630ee24cfaeb6821512aff206afd9c
Sanif Himani <25054137+sanifhimani@users.noreply.github.com>
2019-04-06 21:52:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const firebaseConfig =  {
+    apiKey: "AIzaSyCUrmMuO1ReauJgiWTkyH_tGbItraiGYd0",
+    authDomain: "fuelpe-ff1d8.firebaseapp.com",
+    databaseURL: "https://fuelpe-ff1d8.firebaseio.com",
+    projectId: "fuelpe-ff1d8",
+    storageBucket: "fuelpe-ff1d8.appspot.com",
+    messagingSenderId: "247347328313"
+  };
