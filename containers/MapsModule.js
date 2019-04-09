64628f8768ceb110124bde1113de66ebecb943c7
Ravi Teja Vemuri <vemteja@gmail.com>
2019-04-09 18:10:46 +0530

    maps module ok qr half

--- containers/MapsModule.js
+++ containers/MapsModule.js
@@ -1,152 +1,192 @@
 import React, { Component } from "react";
-import { 
+import {
     View,
     Text,
     StyleSheet,
     Alert,
     FlatList,
     ActivityIndicator,
+    TouchableHighlight
 } from "react-native";
-import { Constants, Location, Permissions,MapView } from 'expo';
+import { Constants, Location, Permissions, MapView } from 'expo';
 import { IntentLauncherAndroid } from 'expo';
 import { data } from '../utils/dummy';
 import axios from 'axios';
 import { mapsApi } from '../config';
 import ListItem from '../components/lists/ListItem'
+import getDirections from 'react-native-google-maps-directions'
 
 
 class MapsModule extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state ={
-            location:{
-                latitude:18.3850,
-                longitude:78.4867
+        this.state = {
+            location: {
+                latitude: 18.3850,
+                longitude: 78.4867
             },
-            errorMessage:null,
-            markers:[],
-            metadata:''
+            errorMessage: null,
+            markers: [],
+            metadata: '',
+            current_dest: ''
         }
-
+        this._showRoute = this._showRoute.bind(this)
     }
- _keyExtractor = (item, index) => item.id;
+    _keyExtractor = (item, index) => item.id;
 
- componentDidUpdate(prevProps,prevState) {
-     const { location } = this.props;
-    if (this.props.location !== prevProps.location) {
-        this.setState({location})
-        this._fetchData(location)
-      
-    }
-  }
-  _fetchData = async (location_state) => {
-      // code for real time request
-      try{
-          if(location_state){
-              //console.log(location_state.latitude, mapsApi)
-              // let orig_data = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location_state.latitude}, ${location_state.longitude}&radius=5000&type=gas_station&key=${mapsApi}`)
-              
-              let cords = data.results.map((point)=>{ // add orig_data. before data for production
-                return point.geometry.location
-            })
-            let metadata = data.results.map((meta)=>{
-                let meta_data = {
-                    name:meta.name,
-                    area:meta.vicinity,
-                    rating:meta.rating,
-                    id:meta.id
-                }
-                return meta_data
-            })
-            //console.log(metadata)
-            this.setState({markers : cords, metadata})
-        } else{
-            console.log("inside else of the fetch data")
-            return false;
+    componentDidUpdate(prevProps, prevState) {
+        const { location } = this.props;
+        if (this.props.location !== prevProps.location) {
+            console.log("from maps", location)
+            this.setState({ location })
+            this._fetchData(location)
+
         }
-    
-    }catch(err){
-        console.log(err);
     }
-    
-}
+    _fetchData = async (location_state) => {
+        // code for real time request
+        try {
+            if (location_state) {
+                console.log(location_state.latitude, mapsApi)
+                // let orig_data = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location_state.latitude}, ${location_state.longitude}&radius=5000&type=gas_station&key=${mapsApi}`)
 
-_handlePress = (index) => {
-    console.log("after press",index)
-}
-
-renderList({item, index}){
-    //console.log(index)
-    return(
-        <ListItem
-        name={item.name}
-        area={item.area}
-        rating={item.rating}
-        key={item.id}
-            index={index}
-            onPress = {this._handlePress}
-            /> 
-    )
-}
-render() {
-    //console.log("state from render",this.state.metadata)
-    
-    return (
-        <React.Fragment >
-    <MapView
-    style={styles.mapContainer}
-    region={{
-        latitude: this.state.location.latitude,
-        longitude: this.state.location.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    }}
-    
-    >
-      {this.props.location ? 
-        <MapView.Marker
-        //key={index}
-        coordinate={this.state.location}
-        title={"me"}
-        pinColor={"teal"}
-        //description={metadata}
-        />:false
-    }
-      
-   {/* {
-       this.state.markers.map((marker, index) => {
-           //console.log("marker",marker)
-           const coords = {
-               latitude: marker.lat,
-               longitude: marker.lng,
-            };
-            
-            //const metadata = `Status: ${marker.statusValue}`;
-            // console.log("corsds are",coords)
-            return (
-                <MapView.Marker
-                key={index}
-                coordinate={coords}
-                title={"petrol"}
-                // description={metadata}
-                />
-            )
-        })} */}
-   </MapView>
-    <View style = {styles.listContainer}>
-        {this.state.metadata ? 
-            <FlatList
-            // pagingEnabled ={true}
-            data={this.state.metadata}
-                //extraData={this.state}
-                renderItem={this.renderList}
-                keyExtractor={this._keyExtractor}
-                />
-                :
-                <Text>No Access to location</Text>
+                let cords = data.results.map((point) => { // add orig_data. before data for production
+                    return point.geometry.location
+                })
+                let metadata = data.results.map((meta) => {
+                    let meta_data = {
+                        name: meta.name,
+                        area: meta.vicinity,
+                        rating: meta.rating,
+                        id: meta.id
+                    }
+                    return meta_data
+                })
+                //console.log(metadata)
+                this.setState({ markers: cords, metadata })
+            } else {
+                console.log("inside else of the fetch data")
+                // Alert.alert(
+                //     'Enable Location',
+                //     'Location is required !',
+                //     [
+                //         {
+                //             text: 'Enable', onPress: async () => {
+                //                 await IntentLauncherAndroid.startActivityAsync(
+                //                     IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS
+                //                 );
+                //             }
+                //         },
+                //     ],
+                //     { cancelable: false }
+                // )
             }
-    </View>
- </React.Fragment>
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    _showRoute = (index) => {
+        console.log("after press", index)
+        const data = {
+            source: {
+                latitude: this.state.location.latitude,
+                longitude: this.state.location.longitude
+            },
+            destination: {
+                latitude: this.state.markers[index].lat,
+                longitude: this.state.markers[index].lng
+            },
+            params: [
+                {
+                    key: "travelmode",
+                    value: "driving"
+                },
+                {
+                    key: "dir_action",
+                    value: "navigate"
+                }
+            ]
+        }
+        console.log('data is ', data)
+        getDirections(data)
+    }
+
+
+    render() {
+        return (
+            <React.Fragment >
+                <MapView
+                    style={styles.mapContainer}
+                    region={{
+                        latitude: this.state.location.latitude,
+                        longitude: this.state.location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+
+                >
+                    {this.props.location ?
+                        <MapView.Marker
+                            //key={index}
+                            coordinate={this.state.location}
+                            title={"me"}
+                            pinColor={"teal"}
+                        //description={metadata}
+                        /> : false
+                    }
+
+                    {
+                        this.state.markers.map((marker, index) => {
+                            //console.log("marker",marker)
+                            const coords = {
+                                latitude: marker.lat,
+                                longitude: marker.lng,
+                            };
+                            return (
+                                <MapView.Marker
+                                    key={index}
+                                    coordinate={coords}
+                                    title={"petrol"}
+                                />
+                            )
+                        })}
+
+                </MapView>
+                <View style={styles.listContainer}>
+                    {this.state.metadata ?
+                        <FlatList
+                            // pagingEnabled ={true}
+                            data={this.state.metadata}
+                            //extraData={this.state}
+                            keyExtractor={this._keyExtractor}
+
+                            renderItem={({ item, index }) => (
+                                <ListItem
+                                    name={item.name}
+                                    area={item.area}
+                                    rating={item.rating}
+                                    key={item.id}
+                                    id={item.id}
+                                    index={index}
+                                    route={this._showRoute}
+                                />
+                            )}
+
+
+                        />
+                        :
+                        <View style={styles.Indicator}>
+                            <Text style={{ color: 'white' }}>Waiting for location</Text>
+                            <ActivityIndicator size="large" color="white" />
+                            {/* <Button   onPress={ () => {this.props.refresh()}}/> */}
+                        </View>
+
+                    }
+                </View>
+            </React.Fragment>
         );
     }
 }
@@ -159,22 +199,21 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         //marginBottom:150
     },
-    listContainer:{
+    listContainer: {
         flex: 1,
-        position:'relative',
-        backgroundColor:'grey',       
+        position: 'relative',
+        backgroundColor: 'grey',
         //bottom:0,
         //padding:3
     },
-    Indicator : {
-        flex:1,
-        justifyContent:'center',
+    Indicator: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
 
     }
 
-
-
+});
 
 
 
@@ -196,12 +235,12 @@ const styles = StyleSheet.create({
     //         ],
     //         { cancelable: false }
     //     )
-        
+
     // } else {
     //     this._getLocationAsync();
     // }
     // }
-    
+
     // _getLocationAsync = async () => {
     //     try{
     //         let { status } = await Permissions.askAsync(Permissions.LOCATION);
@@ -218,13 +257,12 @@ const styles = StyleSheet.create({
     //             longitude:location.coords.longitude
     //         }
     //        // console.log("location state", location_state,"lat and long from location ", location.latitude,location.longitude)
-        
+
     //         this.props.getLocation(location);
     //         this.setState({location:location_state})
     //         this._fetchData(location_state);
-    
+
     //     }catch(err){
     //         console.log(err)
     //     }
     // }
-});
--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -5,7 +5,7 @@ import Swiper from 'react-native-swiper';
 import MapsModule from '../containers/MapsModule';
 import LockModule from '../containers/LockModule';
 import QrModule from '../containers/QrModule';
-import {  Location, Permissions } from 'expo';
+import {  Constants, Permissions, Location  } from 'expo';
 import colors from '../styles/colors';
 
 let index;
@@ -15,37 +15,41 @@ export default class DashboardScreen extends Component {
     constructor(props){
         super(props)
         this.state = {
-            location_state:''
+            location_state:'',
+            errorMessage:null,
         }
     }
      componentWillMount() {
         this._getLocationAsync()
 
     }
-    
+
     _getLocationAsync = async () => {
+        console.log('inside get location')
         try{
             let { status } = await Permissions.askAsync(Permissions.LOCATION);
+            console.log("status", status)
             if(status !== 'granted') {
+                console.log("not granted",this.state.errorMessage)
                 this.setState({
                     errorMessage: 'Permission to access location was denied'
                 });
-                //console.log(this.state.errorMessage)
             }
+
             let location = await Location.getCurrentPositionAsync({});
-            //console.log("location is ", location)
+            console.log("location is ", location)
             let location_state={
                 latitude:location.coords.latitude,
                 longitude:location.coords.longitude
             }
-            //console.log("location state", location_state,"lat and long from location ")
+            console.log("location state", location_state,"lat and long from location ")
         
             //this.props.getLocation(location);
             this.setState({location_state:location_state})
             //this._fetchData(location_state);
     
         }catch(err){
-            console.log(err)
+            console.log(err);
         }
     }
 
@@ -62,12 +66,15 @@ export default class DashboardScreen extends Component {
          nextButton={<Text/>}
     >
         <View style={styles.slide1}>
-          <QrModule/>
+          <QrModule id={this.state.index} />
         </View>
         <View style={styles.slide2}>
-            <LockModule/>
+            <LockModule location_lock = {this.state.location_state}/>
         </View>
-        <MapsModule location={this.state.location_state} />
+        <MapsModule 
+            location={this.state.location_state}
+            refresh = {this._getLocationAsync}
+         />
     </Swiper>
         );
     }
@@ -80,7 +87,7 @@ slide1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.red01,
+    //backgroundColor: colors.red01,
 },
 slide2: {
     flex: 1,
